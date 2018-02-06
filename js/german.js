3df8d40434dad7779dd9221a897295726296b1b6
Sandhya Rao <smudra@yahoo.com>
2018-02-05 23:21:06 -0600

    still working on this file

---
+++ js/german.js
@@ -0,0 +1,57 @@
+"use strict";
+
+/*
+    let toGerman = {
+        translation: 
+    "merry":"fröhliche", 
+    "christmas":"Weihnachten", 
+    "and":"und", 
+    "happy":"frohes", 
+    "new":"neues", 
+    "year":"jahr",
+    "thanksgiving": "Thanksgiving",
+    "happy holidays": "schöne ferien"
+    }
+    get translateToGerman() {
+        if (this.
+    
+        return translateToGerman;
+}
+console.log("Translate to German: ", translateToGerman)
+*/
+
+function toTranslate(inEnglish) {
+return toTranslate;
+}
+
+var fromEnglish = document.getElementById("wordsToTranslate").value;
+fromEnglish = document.getElementById("translatedText").innerHTML;
+console.log("words in the input button", fromEnglish);
+
+/* Next split the string
+var fromEnglishArray = fromEnglish.split((/[, ]+/));
+
+Next loop through the string 
+for(var i = 0; i < length; i++) {
+    if(fromEnglishArray[i] === "merry") {
+        var storeMerry = fromEnglsihArray[i];
+        storeMerry = "fröhliche";
+    } else if
+}
+
+Next 
+
+*/
+
+/*var translateTo = {
+    log: ['a', 'b', 'c'],
+    get latest() {
+      if (this.log.length == 0) {
+        return undefined;
+      }
+      return this.log[this.log.length - 1];
+    }
+  }
+
+
+console.log(translateTo.latest); */
