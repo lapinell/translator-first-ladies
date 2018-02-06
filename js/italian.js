ed86b3ecf053feae3d512ed9e4ab165b11fd1058
Meg Scholl <meghan.e.scholl@gmail.com>
2018-02-05 22:36:21 -0600

    worked on js function for italian language, not finished yet but almost done

---
+++ js/italian.js
@@ -0,0 +1,47 @@
+"use strict";
+
+console.log("test");
+
+
+let italianWords = {
+        "merry": "buon",
+        "christmas": "natale",
+        "and": "e",
+        "happy": "felice",
+        "new": "anno",
+        "year": "nuovo",
+        "valentine": "san valentino",
+        "holiday": "vacanze",
+        "holidays": "vacanze",
+        "halloween": "halloween",
+        "thanksgiving": "ringraziamento",
+        "birthday": "compleanno", 
+        "prosperous": "prospero",
+        "xmas" : "natale",
+        "x-mas" : "natale"
+};
+
+let translatedGreetingArray = [];
+
+let englishGreetingArray = ['Merry', 'Christmas']; //take out
+
+
+function translateToItalian(array) {
+
+    let translatedGreetingArray = italianWords.values();
+    let holidayObjectArray = italianWords.keys(); 
+        
+    for(let i = 0; i > userGreeting.length; i++) {
+        if(userGreeting[i] === holidayObjectArray[i]) {
+            let transWord = translatedGreetingArray[i];
+        } else {
+            console.log("Invalid holiday greeting. Please try a new greeting.");
+        }
+        translatedGreetingArray.push(transWord);
+
+    }
+
+console.log("italian", italianToEnglish);
+return translatedGreetingArray;
+    };
+
