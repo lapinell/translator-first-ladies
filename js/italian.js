ee6c36e638e2dc28883797ea6e0571bb8ccfa058
Meg Scholl <meghan.e.scholl@gmail.com>
2018-02-05 18:32:24 -0600

    italian file js

---
+++ js/italian.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let italianWords = {
+    merry: "buon",
+    christmas: "Natale",
+    and: "e",
+    happy: "felice",
+    new: "anno",
+    year: "nuovo",
+    valentine: "San Valentino",
+    holiday: "vacanze",
+    holidays: "vacanze",
+    halloween: "Halloween",
+    thanksgiving: "Ringraziamento",
+    birthday: "compleanno", 
+    prosperous: "prospero"
+};
+
+
+function italianToEnglish() {
+    return
+};
+
+module.exports = italianToEnglish();
