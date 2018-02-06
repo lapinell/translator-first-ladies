b48d4804a0f306ca0cc3214775d0b09dd5619c39
lapinell <lapinell@gmail.com>
2018-02-05 18:49:46 -0600

    added js language selection

---
+++ js/form.js
@@ -0,0 +1,6 @@
+"use strict";
+
+const spanishSelection = document.getElementById("spanishTranslation").checked; //returns a boolean: true or false
+const frenchSelection = document.getElementById("frenchTranslation").checked;
+const italianSelection = document.getElementById("italianTranslation").checked;
+const germanSelection = document.getElementById("germanTranslation").checked;
